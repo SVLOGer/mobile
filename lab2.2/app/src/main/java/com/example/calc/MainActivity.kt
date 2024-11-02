afd0180304f3ec65a056c232a3c5959eeb20bbff
SVLOGer <max443584@gmail.com>
2024-11-03 01:51:53 +0300

    Поправил 2.2

--- lab2.2/app/src/main/java/com/example/calc/MainActivity.kt
+++ lab2.2/app/src/main/java/com/example/calc/MainActivity.kt
@@ -1,48 +1,43 @@
 package com.example.calc
-import android.graphics.Color
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
-import android.widget.TextView
-import java.lang.Exception
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calc.databinding.ActivityMainBinding
 import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var expressionTextView: TextView
-    private lateinit var resultTextView: TextView
+    private lateinit var binding: ActivityMainBinding
     private var expression = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        expressionTextView = findViewById(R.id.expressionTextView)
-        resultTextView = findViewById(R.id.resultTextView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setUpButtons()
     }
 
     private fun setUpButtons() {
         val buttons = listOf(
-            R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
-            R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9,
-            R.id.btnPlus, R.id.btnMinus, R.id.btnMultiply, R.id.btnDivide,
-            R.id.btnComma, R.id.btnEqual, R.id.btnDelete
+            binding.btn0, binding.btn1, binding.btn2, binding.btn3, binding.btn4,
+            binding.btn5, binding.btn6, binding.btn7, binding.btn8, binding.btn9,
+            binding.btnPlus, binding.btnMinus, binding.btnMultiply, binding.btnDivide,
+            binding.btnComma, binding.btnEqual, binding.btnDelete
         )
 
-        buttons.forEach { buttonId ->
-            val button = findViewById<Button>(buttonId)
+        buttons.forEach { button ->
             button.setOnClickListener { handleButtonClick(button) }
         }
     }
 
-
     private fun handleButtonClick(button: Button) {
         when (button.id) {
-            R.id.btnEqual -> calculateResult()
-            R.id.btnDelete -> {
+            binding.btnEqual.id -> calculateResult()
+            binding.btnDelete.id -> {
                 if (expression.isNotEmpty()) {
                     expression = expression.dropLast(1)
                     updateExpressionText()
@@ -56,7 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateExpressionText() {
-        expressionTextView.text = expression
+        binding.expressionTextView.text = expression
     }
 
     private fun evaluateExpression(expression: String): Pair<String, Boolean> {
@@ -64,24 +59,24 @@ class MainActivity : AppCompatActivity() {
         val exp = Expression(newExpression)
         val result = exp.calculate()
         return if (result.isNaN()) {
-            Pair(getString(R.string.error), true) // true означает ошибку
+            Pair(getString(R.string.error), true)
         } else {
-            Pair(result.toString().replace(".", ","), false) // false означает успешное вычисление
+            Pair(result.toString().replace(".", ","), false)
         }
     }
 
     private fun calculateResult() {
         try {
             val (result, isError) = evaluateExpression(expression)
-            resultTextView.text = result
+            binding.resultTextView.text = result
 
             val textColor = if (isError) Color.RED else Color.BLACK
-            resultTextView.setTextColor(textColor)
-            expressionTextView.setTextColor(textColor)
+            binding.resultTextView.setTextColor(textColor)
+            binding.expressionTextView.setTextColor(textColor)
         } catch (e: Exception) {
-            resultTextView.text = getString(R.string.error)
-            resultTextView.setTextColor(Color.RED)
-            expressionTextView.setTextColor(Color.RED)
+            binding.resultTextView.text = getString(R.string.error)
+            binding.resultTextView.setTextColor(Color.RED)
+            binding.expressionTextView.setTextColor(Color.RED)
         }
     }
 }
